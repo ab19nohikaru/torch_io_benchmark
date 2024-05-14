e4817ff378db99e94a03b245327b197946d3a9f0
unknown <ab19@mail.ustc.edu.cn>
2024-05-14 18:40:15 +0800

    [v0.0.1] benchmark demo in personal pc

---
+++ cmp_tensordict.py
@@ -0,0 +1,229 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+import time, os, platform
+import logging
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+from test_datasets import *
+
+logging.basicConfig(filename="data/log_"+ time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime()) + ".log",
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+def get_available_devices():
+    devices = [(torch.device("cpu"), platform.processor())]
+    n_cuda = torch.cuda.device_count()
+    if n_cuda > 0:
+        for i in range(n_cuda):
+            devices += [(torch.device(f"cuda:{i}"),  torch.cuda.get_device_name(i))]
+    return devices
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+logger.info(f"Available cpus: {os.cpu_count()}")
+logger.info(f"Available devices: {get_available_devices()}")
+logger.info(f"Using device: {device}")
+
+# various dataset preprocessing method
+dataset_preprocess = {"raw":lambda x:x,
+                      "preload":PreloadDataSet,
+                      "tensorclass":lambda dataset:FashionMNISTData.from_dataset(dataset, device=device),
+                      #"memmap":MemmappedDataSet
+                      }
+dl_types = dataset_preprocess.keys()
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            logger.debug(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def tranverse_dataloader(dataloader:DataLoader, epochs:int)->float:
+    t0 = time.time()
+    for t in range(epochs):
+        for data in dataloader:
+            pass
+    return time.time() - t0
+
+def train_tc(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+
+    for batch, data in enumerate(dataloader):
+        X, y = data.images.contiguous(), data.targets.contiguous()
+
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            logger.debug(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+
+            pred = model(X)
+
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+
+    logger.debug(
+        f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+    )
+
+def test_tc(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for batch in dataloader:
+            X, y = batch.images.contiguous(), batch.targets.contiguous()
+
+            pred = model(X)
+
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+
+    logger.debug(
+        f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+    )
+
+
+def cmp_tensorclass_demo(raw_dataset:Dataset, shuffle:bool):
+    batch_size = 64
+    epochs = 5
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    for dl_index, preprocess_type in enumerate(dl_types):
+        if preprocess_type == "tensorclass":
+            collate_fn=lambda x: x
+        else:
+            collate_fn = None
+        training_data = dataset_preprocess[preprocess_type](raw_dataset)
+        dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+        logger.info(f"{preprocess_type.capitalize()} dataloader {'random' if shuffle else 'sequential'} tranverse! time: {tranverse_dataloader(dataloader, epochs): 4.4f} s")
+
+        model = Net().to(device)
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+        t0 = time.time()
+        for t in range(epochs):
+            logger.debug(f"Epoch {t + 1}\n-------------------------")
+            if preprocess_type == "tensorclass":
+                collate_fn=lambda x: x
+                train_tc(dataloader, model, loss_fn, optimizer)
+            else:
+                train(dataloader, model, loss_fn, optimizer)
+        logger.info(f"{preprocess_type.capitalize()} training done! time: {time.time() - t0: 4.4f} s")
+
+def cmp_tranverse_time_for_batch(raw_dataset:Dataset, shuffle:bool):
+    epochs = 1
+    batch_range = range(1, 100)
+    tranverse_time = np.zeros((len(batch_range), len(dl_types)))
+    tranverse_time_dict = {"batch_size": batch_range}
+
+    for dl_index, preprocess_type in enumerate(dl_types):
+        if preprocess_type == "tensorclass":
+            collate_fn=lambda x: x
+        else:
+            collate_fn = None
+        training_data = dataset_preprocess[preprocess_type](raw_dataset)
+        for bs_index, batch_size in enumerate(batch_range):
+            dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+            tranverse_time[bs_index, dl_index] = tranverse_dataloader(dataloader, epochs)
+        tranverse_time_dict[preprocess_type] = tranverse_time[:, dl_index]
+
+    pd.DataFrame(tranverse_time_dict).to_csv(
+                      f"data/{'random' if shuffle else 'sequential'}_tranverse_time_for_batchsize_" + 
+                      f"{int(time.time())}" + ".csv")
+    plt.plot(batch_range, tranverse_time)
+    plt.legend(dl_types)
+    #plt.show()
+
+
+def cmp_tranverse_time_for_epoch(raw_dataset:Dataset, shuffle:bool):
+    batch_size = 64
+    epochs_range = range(1, 11)
+    tranverse_time = np.zeros((len(epochs_range), len(dl_types)))
+    tranverse_time_dict = {"epoch": epochs_range}
+
+    for dl_index, preprocess_type in enumerate(dl_types):
+        if preprocess_type == "tensorclass":
+            collate_fn=lambda x: x
+        else:
+            collate_fn = None
+        training_data = dataset_preprocess[preprocess_type](raw_dataset)
+        dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+        for ep_index, _ in enumerate(epochs_range):
+            tranverse_time[ep_index, dl_index] = tranverse_dataloader(dataloader, 1)
+        tranverse_time_dict[preprocess_type] = tranverse_time[:, dl_index]
+
+    plt.plot(epochs_range, tranverse_time)
+    plt.legend(["raw","preload","tc"])
+    plt.show()
+
+if __name__ == "__main__":
+    raw_training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=False,
+        transform=ToTensor(),
+    )
+    logger.info(f"dataset len {len(raw_training_data)}")
+    cmp_tensorclass_demo(raw_training_data, shuffle=True)
+    #cmp_tensorclass_demo(raw_training_data, shuffle=False)
+    #cmp_tranverse_time_for_batch(raw_training_data, shuffle=True)
+
+logging.shutdown()
---
+++ test_datasets.py
@@ -0,0 +1,93 @@
+import torch
+from torch.utils.data import Dataset
+from tensordict import MemoryMappedTensor
+from tensordict.prototype import tensorclass
+
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+import numpy as np
+from tempfile import mkdtemp
+import os.path as path
+
+class PreloadDataSet(Dataset):
+    def __init__(self, raw_dataset:Dataset) -> None:
+        super().__init__()
+        self.data = []
+        t0 = time.time()
+        for i in range(len(raw_dataset)):
+            self.data.append(raw_dataset[i])
+        logger.info(f"DataSet preload done! time: {time.time() - t0: 4.4f} s")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+class MemmappedDataSet(Dataset):
+    def __init__(self, raw_dataset:Dataset) -> None:
+        super().__init__()
+        self.datafilename = path.join(mkdtemp(), 'pydatafile.dat')
+        self.targetfilename = path.join(mkdtemp(), 'pytargetfile.dat')
+        self.shape = ((len(raw_dataset), *raw_dataset[0][0].squeeze().shape))
+
+        t0 = time.time()
+        data_fp = np.memmap(self.datafilename, dtype='float32', mode='w+', shape=self.shape)
+        target_fp = np.memmap(self.targetfilename, dtype='int64', mode='w+', shape=(self.shape[0],))
+        for i, (image, target) in enumerate(raw_dataset):
+            data_fp[i] = image
+            target_fp[i] = target
+        logger.info(f"DataSet memmap done! time: {time.time() - t0: 4.4f} s")
+
+    def __len__(self):
+        return self.shape[0]
+
+    def __getitem__(self, index):
+        data_fp = np.memmap(self.datafilename, dtype='float32', mode='c',
+                            offset=np.prod(self.shape[1:]) * index * np.dtype(np.float32).itemsize, shape=self.shape[1:])
+        target_fp = np.memmap(self.targetfilename, dtype='int64', mode='c', offset=index * np.dtype(np.int64).itemsize, shape=(1,))
+        return torch.from_numpy(data_fp), target_fp[0]
+
+@tensorclass
+class FashionMNISTData:
+    images: torch.Tensor
+    targets: torch.Tensor
+
+    @classmethod
+    def from_dataset(cls, dataset, device=None):
+        t0 = time.time()
+        data = cls(
+            images=MemoryMappedTensor.empty(
+                (len(dataset), *dataset[0][0].squeeze().shape), dtype=torch.float32
+            ),
+            targets=MemoryMappedTensor.empty((len(dataset),), dtype=torch.int64),
+            batch_size=[len(dataset)],
+            device=device,
+        )
+        for i, (image, target) in enumerate(dataset):
+            data[i] = cls(images=image, targets=torch.tensor(target), batch_size=[])
+        logger.info(f"DataSet preload as tensorclass done! time: {time.time() - t0: 4.4f} s")
+        return data
+
+class MyDataSet(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        self.data = torch.rand([2000,28,28])
+        self.targets = torch.randint(0, 10, (len(self.data),))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        if (index == 0):
+            print("index:0")
+        return self.data[index], self.targets[index]
+
+def my_collate(data):
+    print(len(data), type(data), data[0].shape)
+
+if __name__ == "__main__":
+    pass
