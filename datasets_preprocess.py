86e3cd7363a09f645a21eac400cd8a4e1785bdd8
unknown <ab19@mail.ustc.edu.cn>
2024-05-22 23:57:16 +0800

    [BugFix] fix slow memmap preprocess

--- datasets_preprocess.py
+++ datasets_preprocess.py
@@ -35,21 +35,18 @@ class MemmappedDataSet(Dataset):
         self.shape = ((len(raw_dataset), *raw_dataset[0][0].squeeze().shape))
 
         t0 = time.time()
-        data_fp = np.memmap(self.datafilename, dtype='float32', mode='w+', shape=self.shape)
-        target_fp = np.memmap(self.targetfilename, dtype='int64', mode='w+', shape=(self.shape[0],))
+        self.data_fp = np.memmap(self.datafilename, dtype='float32', mode='w+', shape=self.shape)
+        self.target_fp = np.memmap(self.targetfilename, dtype='int64', mode='w+', shape=(self.shape[0],))
         for i, (image, target) in enumerate(raw_dataset):
-            data_fp[i] = image
-            target_fp[i] = target
+            self.data_fp[i] = image
+            self.target_fp[i] = target
         logger.info(f"DataSet memmap done! time: {time.time() - t0: 4.4f} s")
 
     def __len__(self):
         return self.shape[0]
 
     def __getitem__(self, index):
-        data_fp = np.memmap(self.datafilename, dtype='float32', mode='c',
-                            offset=np.prod(self.shape[1:]) * index * np.dtype(np.float32).itemsize, shape=self.shape[1:])
-        target_fp = np.memmap(self.targetfilename, dtype='int64', mode='c', offset=index * np.dtype(np.int64).itemsize, shape=(1,))
-        return torch.from_numpy(data_fp), target_fp[0]
+        return torch.from_numpy(self.data_fp[index]), self.target_fp[index]
 
 @tensorclass
 class ImageData:
@@ -75,15 +72,13 @@ class ImageData:
 class MyDataSet(Dataset):
     def __init__(self) -> None:
         super().__init__()
-        self.data = torch.rand([2000,28,28])
+        self.data = torch.rand([20,28,28])
         self.targets = torch.randint(0, 10, (len(self.data),))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        if (index == 0):
-            print("index:0")
         return self.data[index], self.targets[index]
 
 # various dataset preprocessing method
@@ -94,4 +89,29 @@ dataset_preprocess_dict = {"raw":lambda x:x,
                       }
 
 if __name__ == "__main__":
-    pass
+    from torch.profiler import profile, record_function, ProfilerActivity
+    import os
+    raw_training_data = MyDataSet()
+    from torchvision import datasets
+    from torchvision.transforms import ToTensor
+    raw_training_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=False,
+        transform=ToTensor(),
+    )
+    save_fir = "log/preprocess_profile"
+    if not os.path.exists(save_fir):
+        os.makedirs(save_fir)
+    logging.basicConfig(handlers=(logging.FileHandler(filename=save_fir + "/self_cpu_time_total.log", mode="w"),),
+                    level=logging.INFO)
+    for preprocess_type, preprocess_method in dataset_preprocess_dict.items():
+        with profile(activities=[ProfilerActivity.CPU],
+            record_shapes=False,
+            profile_memory=True,
+            with_stack=True,
+            ) as prof:
+            with record_function(f"{preprocess_type} dataset"):
+                preprocess_method(raw_training_data)
+        logging.info(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=20))
+        prof.export_chrome_trace(f"{save_fir}/preprocess_{preprocess_type}_trace.json")
